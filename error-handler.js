a0a3f64a804bd4b8d505f1d664c18712b5df45e3
Harry Hornbacher <harry@harry-hornbacher.de>
2018-07-28 22:02:36 +0200

    improve error handling

--- error-handler.js
+++ error-handler.js
@@ -5,15 +5,14 @@
  * @param {object} program Program reference
  */
 global.cli.errorHandler = (message, error = null) => {
-    cli.ui.print(cli.ui.color.info('Usage:'));
-    if (cli.verbosity > 0) cli.program.showHelp();
-    cli.ui.print('\n\n' + cli.ui.color.warn('Error:'));
-
-
     if (message) {
+        cli.ui.print(cli.ui.color.info('Usage:'));
+        if (cli.verbosity > 0) cli.program.showHelp();
+        cli.ui.print('\n\n' + cli.ui.color.warn('Error:'));
         cli.ui.print(cli.ui.color.bold(message));
     }
     else if (error) {
+        cli.ui.print('\n\n' + cli.ui.color.warn('Error:'));
         if (!cli.debugMode) cli.ui.print(cli.ui.color.bold(error.message));
         else cli.ui.print(error);
     }
