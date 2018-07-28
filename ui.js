17ba4e19db961202a77fec9746ec1a763997d57d
Harry Hornbacher <harry@harry-hornbacher.de>
2018-07-28 21:17:18 +0200

    define ui as global in cli

--- ui.js
+++ ui.js
@@ -1,6 +1,5 @@
 const color = require('chalk').default;
 const columnify = require('columnify');
-const clui = require('columnify');
 
 color.warn = color.bold.red;
 color.prompt = color.bold.gray;
@@ -8,25 +7,16 @@ color.info = color.bold.yellow;
 color.userHost = color.bold.green;
 color.path = color.bold.blue;
 
-let verbosity = 1;
-
 const print = (message) => {
-    if (verbosity > 0) console.log(message);
+    if (cli.verbosity > 0) console.log(message);
 };
 
 const printError = (message) => {
-    if (verbosity > 0) console.error(message);
+    if (cli.verbosity > 0) console.error(message);
 };
 
-const setVerbosity = (verbose) => {
-    verbosity = verbose;
-};
 
-const getVerbosity = () => verbosity;
-
-module.exports = {
-    setVerbosity,
-    getVerbosity,
+global.cli.ui = {
     print,
     printError,
     color,
