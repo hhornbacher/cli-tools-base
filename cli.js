29d5a236664456b5ec7757f110f89136c1114c74
Harry Hornbacher <harry@harry-hornbacher.de>
2018-07-28 21:13:09 +0200

    refactor & cleanup

--- cli.js
+++ cli.js
@@ -4,16 +4,21 @@ const _ = require('lodash');
 const path = require('path');
 const fs = require('fs-extra');
 
-// Internal modules
-const config = require('./config');
-const ui = require('./ui');
-const api = require('./api');
+// Define global interface
+global.cli = {
+    program,
+    programName: path.basename(process.argv[1]),
+    debugMode: false,
+    _,
+    fs,
+    verbosity: 1
+};
 
-// Program information
-const programName = path.basename(process.argv[1]);
-
-let debugMode = false;
-
+// Load internal modules
+require('./ui');
+require('./error-handler');
+require('./config');
+require('./api');
 
 const getProgramVersion = () => {
     const getCallerFile = () => {
@@ -47,29 +52,8 @@ const getProgramVersion = () => {
     return require(searchPath).version;
 };
 
-/**
- * General error handler
- * @param {string} message Error message
- * @param {Error} error Error object, that was catched
- * @param {object} program Program reference
- */
-const errorHandler = (message, error, program) => {
-    ui.print(ui.color.info('Usage:'));
-    if (ui.getVerbosity() > 0) program.showHelp();
-    ui.print('\n\n' + ui.color.warn('Error:'));
-    if (debugMode) ui.print(ui.color.bold(error));
-    else ui.print(ui.color.bold(message || error.message));
-    process.exit(1);
-};
-
-global.cli = {
-    config: config(programName),
-    ui,
-    api,
-    _
-};
-
-program.run = () => {
+// Excecute CLI program
+global.cli.run = () => {
     try {
         program.version(getProgramVersion())
             .option('v', {
@@ -77,9 +61,8 @@ program.run = () => {
                 description: 'Show more information',
                 count: true,
                 coerce: (verbose) => {
-                    const v = verbose + 1;
-                    ui.setVerbosity(v);
-                    return v;
+                    global.cli.verbosity += verbose;
+                    return global.cli.verbosity;
                 }
             })
             .option('s', {
@@ -88,7 +71,7 @@ program.run = () => {
                 boolean: true,
                 count: false,
                 coerce: (silent) => {
-                    ui.setVerbosity(0);
+                    global.cli.verbosity = 0;
                     return silent;
                 }
             })
@@ -99,7 +82,8 @@ program.run = () => {
                 count: false,
                 coerce: (debug) => {
                     debugMode = true;
-                    ui.setVerbosity(2);
+                    global.cli.verbosity = 2;
+                    global.cli.debugMode = debugMode;
                     return debug;
                 }
             })
@@ -111,17 +95,15 @@ program.run = () => {
             .completion()
             .strict()
             .wrap(program.terminalWidth())
-            .env(programName.replace(/-/g, '_').toUpperCase())
+            .env(cli.programName.replace(/-/g, '_').toUpperCase())
             .updateStrings({
-                'Commands:': ui.color.info('Commands:'),
-                'Options:': ui.color.info('Options:')
+                'Commands:': cli.ui.color.info('Commands:'),
+                'Options:': cli.ui.color.info('Options:')
             })
-            .fail(errorHandler)
+            .fail(cli.errorHandler)
             .argv;
     }
-    catch (error) {
-        errorHandler(error.message, error, program);
+    catch (err) {
+        cli.errorHandler(null, err, program);
     }
 };
-
-module.exports = program;
