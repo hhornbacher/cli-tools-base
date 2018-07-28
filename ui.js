c84a3b2ca2da77b4716e2786a779f2a281ded103
Harry Hornbacher <harry@harry-hornbacher.de>
2018-07-28 22:02:58 +0200

    improve ui

--- ui.js
+++ ui.js
@@ -1,5 +1,7 @@
 const color = require('chalk').default;
 const columnify = require('columnify');
+const progress = require('cli-progress');
+const ora = require('ora');
 
 color.warn = color.bold.red;
 color.prompt = color.bold.gray;
@@ -7,19 +9,59 @@ color.info = color.bold.yellow;
 color.userHost = color.bold.green;
 color.path = color.bold.blue;
 
-const print = (message) => {
-    if (cli.verbosity > 0) console.log(message);
+const print = (message, type = 'info') => {
+    if (cli.verbosity > 0) {
+        if (type === 'error') {
+            console.error(message);
+        } else {
+            console.log(message);
+        }
+    }
 };
 
-const printError = (message) => {
-    if (cli.verbosity > 0) console.error(message);
+const startProgress = (message) => {
+    if (cli.verbosity > 0) {
+        if (message) {
+            cli.ui._statusSpinner = ora(message).start();
+            console.log('\n');
+        }
+        cli.ui._progressBar = new progress.Bar({}, progress.Presets.shades_classic);
+        cli.ui._progressBar.start(100, 0);
+    }
+};
+
+const updateProgress = (percent, message = null) => {
+    if (cli.verbosity > 0) {
+        cli._progressBar.update(percent);
+
+        if (message) {
+            if (!cli.ui._statusSpinner) {
+                cli.ui._statusSpinner = ora(message).start();
+                console.log('\n');
+            }
+            else {
+                cli.ui._statusSpinner.stop();
+                cli.ui._statusSpinner = ora(message).start();
+                console.log('\n');
+            }
+        }
+    }
+};
+
+const stopProgress = () => {
+    if (cli.verbosity > 0) {
+        cli._progressBar.stop();
+
+        if (cli.ui._statusSpinner) cli.ui._statusSpinner.stop();
+    }
 };
 
 
 global.cli.ui = {
     print,
-    printError,
     color,
     columns: columnify,
-    ui: clui,
+    startProgress,
+    updateProgress,
+    stopProgress
 };
